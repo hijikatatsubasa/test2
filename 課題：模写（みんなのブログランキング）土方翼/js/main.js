25aeca9d90232831c2c52148c9c67ab47806d353
hijikatatsubasa <153829242+hijikatatsubasa@users.noreply.github.com>
2024-01-07 19:38:12 +0900

    test

---
+++ 課題：模写（みんなのブログランキング）土方翼/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    //slick
+    $(".slideItems").slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        infinite: true,
+        slidesToShow:2,
+        slidesToScroll:1,
+      });
+});
