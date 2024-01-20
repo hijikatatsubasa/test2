c67497624a9198552c2bcf48e60b14abae973a4e
hijikatatsubasa <153829242+hijikatatsubasa@users.noreply.github.com>
2024-01-20 21:51:50 +0900

    update

--- 課題：模写（みんなのブログランキング）土方翼/js/main.js
+++ 課題：模写（みんなのブログランキング）土方翼/js/main.js
@@ -7,4 +7,28 @@ $(function() {
         slidesToShow:2,
         slidesToScroll:1,
       });
+    
+    
+    //ハンバーガーメニュー、モーダル
+    $('.hamburger').click(function(){
+        $('.hamburgerModal').fadeIn();
+    });
+
+    $('#close').click(function(){
+        $('.hamburgerModal').fadeOut();
+    });
+
+    $('.pointer').click(function(){
+      var $acordion=$(this).find('.acordion');
+      if($acordion.hasClass('open')) {
+        $acordion.removeClass('open');
+        $acordion.slideUp();
+      } else {
+        $acordion.addClass('open');
+        $acordion.slideDown();
+      }
+
+    });
+    
+    
 });
